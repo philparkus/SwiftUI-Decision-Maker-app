38a908ccb76ed776896e0df970ab136aee56ebc1
HiteshParikh <hiteshparikhus@gmail.com>
2022-03-28 13:46:37 -0400

    Initial Commit

---
+++ level1Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  level1Project
+//
+//  Created by Hitesh Parikh on 3/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ level1Project/level1ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  level1ProjectApp.swift
+//  level1Project
+//
+//  Created by Hitesh Parikh on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct level1ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
